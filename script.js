6da016ac4054b2bf0f94ee1518f3a228bf27c04a
juju01510 <julien.asinari@protonmail.com>
2022-07-01 16:07:08 +0200

    add-start-function

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 let ballRadius = 5;
-let ballSpeed = 5;
 let x = canvas.width / 2;
 let y = canvas.height / 2;
-let dx = ballSpeed;
-let dy = ballSpeed;
+let dx = 8;
+let dy = 2;
 
 let paddleHeight = 60;
 let paddleWidth = 5;
 let paddleY1 = (canvas.height - paddleHeight) / 2;
 let paddleY2 = (canvas.height - paddleHeight) / 2;
-let paddleSpeed = 7;
+let paddleSpeed = 8;
 
 //direction haut/bas paddle1
 let upPressed1 = false;
@@ -27,7 +26,9 @@ let midLineWidth = 5;
 let score1 = 0;
 let score2 = 0;
 
-let colorBall = "blanchedalmond";
+let color = "rgb(255, 235, 205)";
+let colorP1 = "rgb(236, 23, 203)";
+let colorP2 = "rgb(230, 240, 14)";
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -60,20 +61,26 @@ function keyUpHandler(e) {
 
 function drawScore1() {
 	ctx.font = "40px Arial";
-	ctx.fillStyle = "blanchealmond";
+	ctx.fillStyle = colorP1;
 	ctx.fillText(score1, 180, 40);
 }
 
 function drawScore2() {
 	ctx.font = "40px Arial";
-	ctx.fillStyle = "blanchealmond";
+	ctx.fillStyle = colorP2;
 	ctx.fillText(score2, 275, 40);
+}
+
+function drawStart() {
+	ctx.font = "35px Arial";
+	ctx.fillStyle = color;
+	ctx.fillText("-- Press Spacebar to play --", 25, canvas.height / 2 + 10);
 }
 
 function drawBall() {
 	ctx.beginPath();
 	ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-	ctx.fillStyle = colorBall;
+	ctx.fillStyle = color;
 	ctx.fill();
 	ctx.closePath();
 }
@@ -81,7 +88,7 @@ function drawBall() {
 function drawPaddle1() {
 	ctx.beginPath();
 	ctx.rect(5, paddleY1, paddleWidth, paddleHeight);
-	ctx.fillStyle = "blanchedalmond";
+	ctx.fillStyle = colorP1;
 	ctx.fill();
 	ctx.closePath();
 }
@@ -89,7 +96,7 @@ function drawPaddle1() {
 function drawPaddle2() {
 	ctx.beginPath();
 	ctx.rect(-5 + canvas.width - paddleWidth, paddleY2, paddleWidth, paddleHeight);
-	ctx.fillStyle = "blanchedalmond";
+	ctx.fillStyle = colorP2;
 	ctx.fill();
 	ctx.closePath();
 }
@@ -97,7 +104,7 @@ function drawPaddle2() {
 function drawMidline() {
 	ctx.beginPath();
 	ctx.rect(canvas.width / 2 - midLineWidth / 2, 0, midLineWidth, midLineHeight);
-	ctx.fillStyle = "blanchealmond";
+	ctx.fillStyle = "rgb(255, 235, 205)";
 	ctx.fill();
 	ctx.closePath();
 }
@@ -115,8 +122,9 @@ function draw() {
 	if (x + dx < ballRadius) {
 		if (y > paddleY1 && y < paddleY1 + paddleHeight) {
 			dx = -dx;
+			color = colorP1;
 		} else {
-			//remet la balle en jeu au milieu du terrain
+			//remet la balle en jeu
 			x = canvas.width;
 			y = canvas.height / 2;
 			score2++;
@@ -130,8 +138,9 @@ function draw() {
 	} else if (x + dx > canvas.width - ballRadius) {
 		if (y > paddleY2 && y < paddleY2 + paddleHeight) {
 			dx = -dx;
+			color = colorP2;
 		} else {
-			//remet la balle en jeu au milieu du terrain
+			//remet la balle en jeu
 			x = canvas.width - canvas.width;
 			y = canvas.height / 2;
 			score1++;
@@ -162,4 +171,15 @@ function draw() {
 
 	requestAnimationFrame(draw);
 }
-draw();
+// draw();
+drawStart();
+
+document.addEventListener("keypress", start);
+
+function start(e) {
+	if (e.keyCode == 32) {
+		draw();
+
+		document.removeEventListener("keypress", start);
+	}
+}
